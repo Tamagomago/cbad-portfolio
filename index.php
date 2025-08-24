099d77acd106223f8ce542680d72307f52298887
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-24 22:05:07 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Raean.dev</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid g-0 min-vh-100 d-flex align-items-center justify-content-center">
+  <!-- sidebar (3 cols), main content (9 cols) -->
+  <div class="row g-0 w-100">
+    <aside class="col-12 col-md-3 h-lg-100">
+      <?php include __DIR__ . '/menu.php'; ?>
+    </aside>
+    <main class="col-12 col-md-9 d-flex align-items-center justify-content-center">
+      <div class="design-surface d-flex align-items-center justify-content-center position-relative w-100">
+        <!-- design surface -->
+        <div class="overlay position-absolute"></div>
+        <div class="d-flex align-items-stretch">
+          <!-- Image placeholder -->
+          <div class="placeholder-box bg-secondary"></div>
+          
+          <!-- Content -->
+          <div class="content-box d-flex flex-column justify-content-center px-4">
+            <h1 class="fs-2">Raean Chrissean R. Tamayo</h1>
+            <h1 class="fs-4 txt-muted">Fullstack Developer</h1>
+            <!-- Location -->
+            <div class="d-flex align-items-center gap-2 mb-3">
+              <div class="loc-img d-flex items-center justify-center">
+                <img src="assets/icons/loc.svg" alt="location" class="img-fluid">
+              </div>
+              <a href="https://share.google/WoNZNqpzuqZwmlUbf" target="_blank" class="txt-muted fs-5 loc-link">
+                Philippines
+              </a>
+            </div>
+            <!-- See my works button -->
+            <div class="d-flex">
+              <button type="button" class="btn btn-outline-light w-auto d-flex align-items-center gap-2">
+                See my works
+              </button>
+            </div>
+            <!-- About me -->
+            <div class="about-me mt-4">
+              <p class="fs-6 txt-muted">
+                I’m a Computer Science student at the University
+                of Saint La Salle – Bacolod. I enjoy building clean and user-friendly applications. </p>
+            </div>
+          </div>
+        </div>
+      </div> <!-- close .design-surface -->
+    </main>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<div class="sidebar d-flex flex-column justify-content-center align-items-center text-center py-4">
+  <!--  <p class="txt-muted fs-1 mb-5">Raean.dev</p>-->
+  <nav class="nav flex-column align-items-center gap-2">
+    <a href="#about" class="sidebar-items">About</a>
+    <a href="#projects" class="sidebar-items">Skills & Projects</a>
+    <a href="#experience" class="sidebar-items">Experience</a>
+    <a href="#contact" class="sidebar-items">Contact</a>
+  </nav>
+</div>
