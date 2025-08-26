e4314d86f51c81f015f1ff844efeff9eae1ad87a
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-08-26 13:43:49 +0800

    chore: change behaviour of some styles

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <header class="sidebar d-flex flex-column justify-content-center align-items-center text-center py-4 h-md-100">
-  <nav class="align-items-center gap-2 gap-md-3 gap-lg-4">
+  <nav class="align-items-center ">
     <a href="index.php" class="sidebar-items">Home</a>
     <a href="tools.php" class="sidebar-items">Current Tools</a>
     <a href="projects.php" class="sidebar-items">My Projects</a>
--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded and parsed");
   const menuLinks = document.querySelectorAll(".sidebar-items");
   let currentPath = window.location.pathname.split("/").pop();
-
-  // Default to index.php if path is empty (root)
   if (!currentPath) currentPath = "index.php";
 
+  // Find the current menu link
+  let currentLink = null;
   menuLinks.forEach((link) => {
     const linkPath = link.getAttribute("href");
     if (linkPath === currentPath) {
-      console.log(linkPath);
       link.classList.add("selected");
+      currentLink = link;
     } else {
       link.classList.remove("selected");
     }
+
+    link.addEventListener("mouseenter", function () {
+      menuLinks.forEach((l) => l.classList.remove("selected"));
+    });
+
+    link.addEventListener("mouseleave", function () {
+      if (currentLink) currentLink.classList.add("selected");
+    });
   });
 });
